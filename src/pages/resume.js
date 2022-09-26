ec1c0e45d93375014ab072e97b076141ba80c2e6
Satyajit <satyajit12.roy@gmail.com>
2022-09-26 22:41:04 +0530

    First push

---
+++ src/pages/resume.js
@@ -0,0 +1,11 @@
+export default function Resume() {
+  return (
+    <></>
+    // <PDFViewer
+
+    //   document={{
+    //     url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+    //   }}
+    // />
+  );
+}
